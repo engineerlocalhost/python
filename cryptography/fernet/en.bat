521153fcd9e6d72b2eb4ef6f94c6f630b5c978f4
engineerlocalhost <engineerlocalhost@gmail.com>
2024-08-29 23:47:43 +0700

    c

---
+++ cryptography/fernet/en.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Ganti dengan path ke direktori Python Anda jika perlu
+set PYTHON_PATH=C:\Users\captennem0\AppData\Local\Programs\Python\Python312
+
+REM Ganti dengan path ke file skrip Python Anda
+set SCRIPT_PATH=D:\en.py
+
+REM Menjalankan skrip Python
+"%PYTHON_PATH%\python.exe" "%SCRIPT_PATH%"
+
+REM Menunggu agar pengguna dapat melihat hasil
+pause
